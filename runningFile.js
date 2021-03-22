6c375d2686f2347d7a1d6042b284b2eeda3f0719
bats1996 <bats1996@gmail.com>
2021-03-22 14:13:14 +0000

    adding grab

---
+++ runningFile.js
@@ -0,0 +1,39 @@
+let x, y; 
+let go=false;
+let img;
+
+function preload() {
+  img = loadImage('folder.png');
+}
+ 
+ function setup() { 
+  createCanvas(windowWidth, windowHeight); 
+  imageMode(CENTER);
+  newPosition();
+} 
+ 
+function draw() { 
+  background(128);
+  cursor('grab');
+  let p = createVector( mouseX-x, mouseY-y, 0);
+  p.limit(2);
+  if (go && dist(mouseX, mouseY, x, y)<100) {
+    x-=p.x;
+    y-=p.y;
+  }
+  x = constrain(x,25,width-25);
+  y = constrain(y,25,height-25);
+  image(img, x, y, 50, 50); 
+  if (mouseX>x-25 && mouseX<x+25 && mouseY>y-25 && mouseY<y+25) {
+    newPosition();
+  }
+}
+ 
+function mouseMoved() {
+  go=true;
+}
+ 
+function newPosition() {
+  x=random(width); 
+  y=random(height);
+}
